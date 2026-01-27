4cd80b0af77c28b468b1ab5e566fbcb79e5dba69
赵岩 <zy@zydeMacBook-Air.local>
2026-01-28 00:29:56 +0800

    架构更新

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -10,25 +10,37 @@ export default defineConfig({
     // 顶部导航栏（可选）
     nav: [
       { text: '首页', link: '/' },
-      { text: '开始阅读', link: '/novels/小说《铸日》' } // 指向你的第一章
+      { text: '开始阅读', link: '/novels/zhuri/zhuri1' } // 指向你的第一章
     ],
 
     // 侧边栏核心配置
     sidebar: [
       {
-        text: '幻想造物', // 目录分组标题
+        text: '铸日', // 目录分组标题
         collapsed: false,      // 是否默认折叠
         items: [
           // 这里填你的文件名，link 对应文件路径（不需要写 .md）
-          { text: '一 铸日', link: '/novels/小说《铸日》' },
-          { text: '二 时间', link: '/novels/时间魔法' },
+          { text: '一', link: '/novels/zhuri/zhuri1' },
+          { text: '二', link: '/novels/zhuri/zhuri2' },
+          { text: '三', link: '/novels/zhuri/zhuri3' },
+          { text: '四', link: '/novels/zhuri/zhuri4' },
+        ]
+      },
+      {
+        text: '时间魔法', // 目录分组标题
+        collapsed: false,      // 是否默认折叠
+        items: [
+          // 这里填你的文件名，link 对应文件路径（不需要写 .md）
+          { text: '一', link: '/novels/timemagic/timemagic1' },
+          { text: '二', link: '/novels/timemagic/timemagic2' },
+          { text: '三', link: '/novels/timemagic/timemagic3' },
         ]
       }
     ],
 
     // 社交链接图标
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/你的githubID' }
+      { icon: 'github', link: 'https://github.com/yanzhao1027' }
     ]
   }
 })
